2c1902a8c97131cd54c1db866591a73e99cef7ad
freire-guido <14301656+freire-guido@users.noreply.github.com>
2025-01-26 17:06:55 -0300

    types

--- options.py
+++ options.py
@@ -6,5 +6,11 @@ def fetch_binance_options():
     response = requests.get(url)
     response.raise_for_status()
     if response.status_code == 200:
-        data = response.json()
-        return pd.DataFrame(data)
+        data = pd.DataFrame(response.json())
+        data['bidPrice'] = data['bidPrice'].astype(float)
+        data['askPrice'] = data['askPrice'].astype(float)
+        data['strikePrice'] = data['strikePrice'].astype(float)
+        data['amount'] = data['amount'].astype(float)
+        data['volume'] = data['volume'].astype(float)
+        data['exercisePrice'] = data['exercisePrice'].astype(float)
+        return data
